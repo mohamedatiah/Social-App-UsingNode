b313944016b791981ca6a4e3a282faf91d3ac26b
mohamedatiah <mohamedatiah1997@gmail.com>
2021-01-25 00:13:18 +0200

    update2

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const postRoute = require('./routes/post');
 const userRoute = require('./routes/user');
 const authMiddleware = require('./middlewares/auth');
 const app = express();
-mongoose.connect('mongodb://localhost:27017/SD',{useNewUrlParser: true, useUnifiedTopology: true});
+const {  MONGODB_URI} = process.env
+mongoose.connect( MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(express.json());
 
